5724c8b362660dcdcb8fc64b95f5414f13ee8398
EvteeevArtem <essen26@gmail.com>
2018-03-26 23:42:53 +0300

    add header file for eightbit counter

---
+++ register/counter.h
@@ -0,0 +1,36 @@
+#include <systemc.h>
+
+
+SC_MODULE(eightbit_register_counter) {
+
+	sc_in_clk clock;
+	sc_in <bool> reset;
+	sc_in <bool> load;
+	sc_in <bool> synch_reset;
+	sc_out<sc_uint<8> > register_out;
+
+
+	sc_signal<sc_uint<8> > myregister;
+
+
+	void register_store();
+
+	void data_out_method();
+
+
+	SC_CTOR(eightbit_register_counter) :
+		clock("clock"),
+		reset("reset"),
+		load("load"),
+		synch_reset("synch_reset"),
+		register_out("register_out")
+
+	{
+		cout << "Executing new" << endl;
+
+		SC_CTHREAD(register_store, clock.pos());
+		async_reset_signal_is(reset, true);
+		SC_METHOD(data_out_method);
+		sensitive << myregister;
+	}
+};
